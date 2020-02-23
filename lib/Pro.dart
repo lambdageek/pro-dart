488632432ed321bf0abd0c9a396cd92f4c1e8ccc
Aleksey Kliger <aleksey@lambdageek.org>
2020-02-22 20:07:05 -0500

    Embellish example. move some subscription pauses to .then
    Not sure why, but this helps to make it so that the yielding counters
    print only one value per tick.  previously when the pause was in
    onData, the counters would print twice each.

--- lib/Pro.dart
+++ lib/Pro.dart
@@ -4,12 +4,16 @@ import 'dart:convert' as convert;
 import 'dart:collection';
 import 'package:meta/meta.dart';
 
+Future<void> pause() {
+  return Future.delayed(Duration(seconds: 0), () {});
+}
+
 class UnreachableError extends Error {}
 
 @alwaysThrows
 T unreachable<T>() {
-  assert  (false, "Unreachable code");
-  throw new UnreachableError ();
+  assert(false, "Unreachable code");
+  throw new UnreachableError();
 }
 
 /*
@@ -40,21 +44,25 @@ class Signal {
   Completer<void> _completer;
   bool _alreadySignaled;
 
-  Signal() : _alreadySignaled = false, _completer = Completer<void>();
+  Signal()
+      : _alreadySignaled = false,
+        _completer = Completer<void>();
 
-  void reset () {
+  void reset() {
     _alreadySignaled = false;
     _completer = Completer<void>();
   }
 
-  void signal () {
+  void signal() {
     if (!_alreadySignaled) {
       _alreadySignaled = true;
-      _completer.complete (null);
+      _completer.complete(null);
     }
   }
 
-  Future<void> get future => _completer.future;
+  Future<void> get future => _completer.future.then((_) {
+        reset();
+      });
 }
 
 /// A scheduler supervises several processes and periodically allows them to run
@@ -65,35 +73,38 @@ class Scheduler {
   /// is paused.  When a process is [State.Dead] it is removed from the queue;
   /// when a process is blocked, it is removed from the queue and will be put
   /// back in when it is ready again.
-  final Queue<ScheduledProcess>  ready;
+  final Queue<ScheduledProcess> ready;
+
   /// Every process that this scheduler is responsible for.  When the dispatcher is done, every subscription
   Iterator<ScheduledProcess> get everyProcess => _processes.iterator;
 
   List<ScheduledProcess> _processes;
-  
+
   int _dead;
 
-  Scheduler() : ready = Queue<ScheduledProcess>() , _processes = [], _dead = 0, blockedStateChange = Signal();
+  Scheduler()
+      : ready = Queue<ScheduledProcess>(),
+        _processes = [],
+        _dead = 0,
+        blockedStateChange = Signal();
 
   int get dead => _dead;
   int get numProcesses => _processes.length;
 
-  void spawn (Process process) {
-    var sp = ScheduledProcess (process);
-    _processes.add (sp);
-    assert (process.state == State.Running);
-    ready.addLast (sp);
+  void spawn(String name, Process process) {
+    var sp = ScheduledProcess(name, process);
+    _processes.add(sp);
+    assert(process.state == State.Running);
+    ready.addLast(sp);
   }
 
   Signal blockedStateChange;
-  
-  void dispatch () async {
+
+  void dispatch() async {
     while (dead < numProcesses) {
       while (ready.isEmpty) {
-        
-        print ("ready is empty");
+        print("ready is empty");
         await blockedStateChange.future;
-        blockedStateChange.reset();
         if (dead == numProcesses) {
           print("all processes dead, returning");
           return;
@@ -101,24 +112,32 @@ class Scheduler {
       }
 
       final p = ready.removeFirst();
-      assert (p.process.state == State.Running);
+      assert(p.process.state == State.Running);
+      final n = p.name;
+      print("process $n, ready to run");
       await p.run();
       final s = p.process.state;
-      print ("process, ran, new state is $s");
+      print("process $n ran, new state is $s");
       switch (s) {
         case State.Dead:
-        p.close();
-        _dead++;
-        break;
+          p.close();
+          _dead++;
+          break;
         case State.Running:
-        ready.addLast(p);
-        break;
+          ready.addLast(p);
+          break;
         case State.Blocked:
-        p.doBlocking (onReady: () { blockedStateChange.signal(); ready.addLast (p); print ("process is ready"); });
-        break;
+          p.doBlocking(onReady: () {
+            blockedStateChange.signal();
+            ready.addLast(p);
+            print("process $n is ready");
+          });
+          break;
       }
       final readyState = ready.isEmpty ? "empty" : "not empty";
-      print ("end of scheduler iteration: dead = $dead, ready = $readyState, numProcesses = $numProcesses");
+      print(
+          "end of scheduler iteration: dead = $dead, ready = $readyState, numProcesses = $numProcesses");
+      await pause();
     }
   }
 }
@@ -130,49 +149,58 @@ class Scheduler {
 /// [ScheduledProcess.doBlocking] method kicks off an asynchronous task that
 /// will wait to receive a tick from the process when it is no longer blocking
 /// and then add it back to the scheduler's ready queue.
-@immutable @sealed
+@immutable
+@sealed
 class ScheduledProcess {
+  final String name;
   final Process process;
   final StreamSubscription<Tick> subscription;
 
-  ScheduledProcess (this.process) : subscription = process.stream.listen(null)..pause();
+  ScheduledProcess(this.name, this.process)
+      : subscription = process.stream.listen(null)..pause();
 
-  Future<Tick> run () {
-    Completer<Tick> step = Completer<Tick> ();
+  Future<Tick> run() {
+    Completer<Tick> step = Completer<Tick>();
     subscription
-    ..onDone (() {
-        step.complete (Tick.Tock);
-    })
-    ..onData ((Tick tick) {
+      ..onDone(() {
+        print("run: done $name");
+        step.complete(Tick.Tock);
+      })
+      ..onData((Tick tick) {
         // FIXME: xxx Is it safe to call pause from an onData handler?
-        subscription.pause ();                 
-        step.complete (tick);
-    })
-    ..resume ();
-    return step.future;
+        //subscription.pause ();
+        step.complete(tick);
+      })
+      ..resume();
+    print("run: resumed $name");
+    return step.future.then((tick) {
+      subscription.pause();
+      print("run: paused $name");
+      return tick;
+    });
   }
 
   void close() {
     subscription.cancel();
   }
 
-  void doBlocking ({@required void onReady()}) {
-    void runBlocking () {
-      print ("in runBlocking");
+  void doBlocking({@required void onReady()}) {
+    void runBlocking() {
+      print("in runBlocking of $name");
       subscription
-      ..onDone (() {
-          print ("in blocking onDone");
+        ..onDone(() {
+          print("in blocking onDone of $name");
           onReady();
-      })
-      ..onData ((Tick tick) {
-          print ("in blocking onData");
-          subscription.pause ();
+        })
+        ..onData((Tick tick) {
+          print("in blocking onData of $name");
+          subscription.pause();
           onReady();
-      })
-      ..resume ();
+        })
+        ..resume();
     }
 
-    Timer.run (runBlocking);
+    Timer.run(runBlocking);
   }
 }
 
@@ -240,34 +268,72 @@ StreamTransformer<A, S> unfoldAutomaton<S, A>(
 
 @sealed
 class LineEchoProcess implements Process {
-  LineEchoProcess () : _state = State.Running;
+  LineEchoProcess() : _state = State.Running;
 
   @override
   State get state => _state;
 
   @override
-  Stream<Tick> get stream { _stream ??= initStream(); return _stream; }
+  Stream<Tick> get stream {
+    _stream ??= initStream();
+    return _stream;
+  }
 
   Stream<Tick> _stream;
   State _state;
 
-  Stream<Tick> initStream () async* {
-    final stdinLines = io.stdin.transform (convert.utf8.decoder).transform (const convert.LineSplitter());
+  Stream<Tick> initStream() async* {
+    final stdinLines = io.stdin
+        .transform(convert.utf8.decoder)
+        .transform(const convert.LineSplitter());
 
     _state = State.Blocked;
     yield Tick.Tock;
     await for (var line in stdinLines) {
       _state = State.Running;
       yield Tick.Tock;
-      print ("Running: have $line");
+      print("Running: have $line");
       _state = State.Blocked;
       yield Tick.Tock;
     }
     _state = State.Dead;
     yield Tick.Tock;
   }
-
 }
+
+@sealed
+class YieldingCounter implements Process {
+  YieldingCounter(this.name, this.max) : _state = State.Running;
+
+  @override
+  State get state => _state;
+
+  @override
+  Stream<Tick> get stream {
+    _stream ??= initStream();
+    return _stream;
+  }
+
+  Stream<Tick> _stream;
+  State _state;
+
+  int max;
+  String name;
+
+  Stream<Tick> initStream() async* {
+    int count = 0;
+
+    while (count < max) {
+      print("Counter ($name): $count");
+      ++count;
+      _state = State.Running;
+      yield Tick.Tock;
+    }
+    _state = State.Dead;
+    yield Tick.Tock;
+  }
+}
+
 void runner() async {
   print("hello world!\n");
 
@@ -281,11 +347,11 @@ void runner() async {
   stream.transform(unfoldAutomaton(step, 0)).take(5).listen(print);
   */
 
-  var scheduler = new Scheduler ();
+  var scheduler = new Scheduler();
 
-  scheduler.spawn (new LineEchoProcess());
+  scheduler.spawn("echo", new LineEchoProcess());
+  scheduler.spawn("count a", new YieldingCounter('a', 10));
+  scheduler.spawn("count b", new YieldingCounter('b', 10));
 
-  await scheduler.dispatch ();
-
-  
+  await scheduler.dispatch();
 }
